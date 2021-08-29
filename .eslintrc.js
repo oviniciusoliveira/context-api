5c4ca2914dbe13936f620345f36c84eff7911e76
Vinícius Oliveira <viniciuso.contato@gmail.com>
2021-08-28 22:12:43 -0300

    :seedling: first commit

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  settings: { react: { version: 'detect' } },
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'react/react-in-jsx-scope': 'off',
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  trailingComma: "all",
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "always",
+  endOfLine: "lf",
+};
