68eac8d3193587721a44a5c4a2c0ff6a998cbc0a
Vinícius Oliveira <viniciuso.contato@gmail.com>
2021-08-29 21:49:33 -0300

    :sparkles: add reset, set, async increase, async error and handleError features

--- src/contexts/CounterContext/actionFactory.js
+++ src/contexts/CounterContext/actionFactory.js
@@ -4,5 +4,31 @@ export const actionFactory = (dispatch) => {
   return {
     increase: () => dispatch({ type: actionTypes.INCREASE }),
     decrease: () => dispatch({ type: actionTypes.DECREASE }),
+    reset: () => dispatch({ type: actionTypes.RESET }),
+    setCounter: (payload) => dispatch({ type: actionTypes.SET_COUNTER, payload }),
+    asyncIncrease: () => asyncIncreaseFn(dispatch),
+    asyncError: () => asyncErrorFn(dispatch),
   };
+};
+
+const asyncIncreaseFn = async (dispatch) => {
+  dispatch({ type: actionTypes.ASYNC_INCREASE_START });
+
+  return await new Promise((resolve) => {
+    setTimeout(() => {
+      dispatch({ type: actionTypes.ASYNC_INCREASE_END });
+      resolve('Promise Resolved');
+    }, 2000);
+  });
+};
+
+const asyncErrorFn = async (dispatch) => {
+  dispatch({ type: actionTypes.ASYNC_INCREASE_START });
+
+  return await new Promise((resolve, reject) => {
+    setTimeout(() => {
+      dispatch({ type: actionTypes.ASYNC_INCREASE_ERROR });
+      reject(new Error('Promise Error!'));
+    }, 2000);
+  });
 };
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -5,12 +5,25 @@ import { useCounterContext } from '../../contexts/CounterContext';
 
 function Home() {
   const [state, actions] = useCounterContext();
+
+  const handleError = () => {
+    actions
+      .asyncError()
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error.name, ':', error.message));
+  };
+
   return (
     <div>
       <Heading />
       <div>
         <Button text="Increase" onButtonClick={actions.increase} />
         <Button text="Decrease" onButtonClick={actions.decrease} />
+        <Button text="Reset" onButtonClick={actions.reset} />
+        <Button text="Set 10" onButtonClick={() => actions.setCounter({ counter: 10 })} />
+        <Button text="Set 100" onButtonClick={() => actions.setCounter({ counter: 100 })} />
+        <Button disabled={state.loading} text="Async Increase" onButtonClick={actions.asyncIncrease} />
+        <Button disabled={state.loading} text="Async Error" onButtonClick={handleError} />
       </div>
     </div>
   );
